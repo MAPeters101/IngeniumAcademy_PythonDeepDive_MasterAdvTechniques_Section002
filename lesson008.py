2e88ae887bcc4b9b5435b0a474edfa34a4105ef3
Mark Peters <mapeters101@earthlink.net>
2025-02-15 17:46:55 -0500

    Work on lesson008 to 3:00.

---
+++ lesson008.py
@@ -0,0 +1,27 @@
+"""
+Basic Representation: __repr__ and __str__
+
+These methods determine the human-readable and unambiguous string
+representation of an object
+"""
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __repr__(self): # for developers
+        return f"Book('{self.title}',  '{self.author}')"
+
+    def __str__(self): # for end user
+        return f"'{self.title}' by {self.author}"
+
+
+if __name__ == '__main__':
+    book = Book("1984", "George Orwell")
+    print(repr(book))
+
+    #print(eval(repr(book)).title)
+
+    print(str(book))
+
