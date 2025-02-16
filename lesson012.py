1c81695e06771e9ac8c5699ce604c139d895b15c
Mark Peters <mapeters101@earthlink.net>
2025-02-16 14:36:03 -0500

    Complete work on lesson012 from 1:00.  class Library.

--- lesson012.py
+++ lesson012.py
@@ -5,12 +5,17 @@ Enable the built-in len() function for custom objects.
 """
 
 class Library:
-    def __init__(self, books):
+    def __init__(self, books, librarians):
         self.books = books
+        self.librarians = librarians
 
     def __len__(self):
-        return len(self.books)
+        return len(self.librarians) + len(self.books)
 
 
+if __name__ == '__main__':
+    lib = Library(books=["book1", "book2"],
+                  librarians=['librarian1', 'librarian2'])
+    print(len(lib))
 
 
