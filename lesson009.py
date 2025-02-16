f8b276a6800d0fa040ec3a999f816e6948d3c483
Mark Peters <mapeters101@earthlink.net>
2025-02-16 12:26:45 -0500

    Complete work on lesson009 from 2:00.  '__main__'.

--- lesson009.py
+++ lesson009.py
@@ -15,10 +15,22 @@ class ComplexNumber:
 
     def __mul__(self, other):
         real_part = self.real * other.real - self.imag * other.imag
-        imag_part = self.imag * other.real - self.real * other.imag
+        imag_part = self.imag * other.real + self.real * other.imag
         return ComplexNumber(real_part, imag_part)
 
 
+if __name__ == '__main__':
+    # Create two complex numbers
+    c1 = ComplexNumber(3, 2)
+    c2 = ComplexNumber(1, 7)
+
+    # Add the two complex numbers using the __add__ method
+    sum_result = c1 + c2
+    print(f'Sum: {sum_result.real} + {sum_result.imag}i')
+
+    # Multiply the two complex numbers using the __mul__ method
+    product_result = c1 * c2
+    print(f'Product: {product_result.real} + {product_result.imag}i')
 
 
 
