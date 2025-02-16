67526ba52a61e6fdae9352acfd916b55a16917bf
Mark Peters <mapeters101@earthlink.net>
2025-02-16 17:25:16 -0500

    Work on lesson013 to 1:00.  class Countdown.

---
+++ lesson013.py
@@ -0,0 +1,25 @@
+"""
+Object Iteration: iter and next
+
+Make custom objects iterable using loops
+"""
+
+class Countdown:
+    def __init__(self, start):
+        self.current = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= 0:
+            raise StopIteration
+        self.current -= 1
+        return self.current + 1
+
+
+
+
+
+
+
