cdb5a6bd55f8a3a46fbeff9a0f3c0a984a67d551
Mark Peters <mapeters101@earthlink.net>
2025-02-16 17:31:09 -0500

    Complete work on lesson013 from 1:00.  __main__.

--- lesson013.py
+++ lesson013.py
@@ -3,6 +3,8 @@ Object Iteration: iter and next
 
 Make custom objects iterable using loops
 """
+from itertools import count
+
 
 class Countdown:
     def __init__(self, start):
@@ -18,8 +20,14 @@ class Countdown:
         return self.current + 1
 
 
+if __name__ == '__main__':
 
 
+    # Create an instance of Countdown starting from 5
+    countdown_from_5 = Countdown(5)
 
+    # Iterate over the countdown using a for loop
+    for number in countdown_from_5:
+        print(number)
 
 
